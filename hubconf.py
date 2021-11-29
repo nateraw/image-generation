c4442c4b9c33d280e1466e5d082b5040ae8d7fa7
nateraw <nxr9266@g.rit.edu>
2021-11-29 14:42:19 -0500

    :construction: wip

--- hubconf.py
+++ hubconf.py
@@ -1,20 +1,17 @@
-# import gdown
+import gdown
 
-# from huggingface_stylegan3 import StyleGAN3ImageGenerationPipeline
+from huggingface_stylegan3 import StyleGAN3ImageGenerationPipeline
 
-# model_map = {
-#     'art': 'https://drive.google.com/uc?id=18MOpwTMJsl_Z17q-wQVnaRLCUFZYSNkj',
-#     'landscapes': 'https://drive.google.com/uc?id=14UGDDOusZ9TMb-pOrF0PAjMGVWLSAii1'
-# }
+model_map = {
+    'art': 'https://drive.google.com/uc?id=18MOpwTMJsl_Z17q-wQVnaRLCUFZYSNkj',
+    'landscapes': 'https://drive.google.com/uc?id=14UGDDOusZ9TMb-pOrF0PAjMGVWLSAii1'
+}
 
-# def styleganv3_inference(model: str = 'art'):
-#     if model.startswith('http'):
-#         url = model
-#     elif model in model_map:
-#         url = model_map.get(model, None)
-#     else:
-#         raise ValueError(f'Model {model} not found')
-#     return StyleGAN3ImageGenerationPipeline(url)
-
-def asdf():
-    return 'asdf'
+def styleganv3_inference(model: str = 'art'):
+    if model.startswith('http'):
+        url = model
+    elif model in model_map:
+        url = model_map.get(model, None)
+    else:
+        raise ValueError(f'Model {model} not found')
+    return StyleGAN3ImageGenerationPipeline(url)
