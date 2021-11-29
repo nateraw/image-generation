788549f59f718a98d6d8f3f949500dae6b414ed3
nateraw <nxr9266@g.rit.edu>
2021-11-29 14:55:32 -0500

    :construction: wip

--- hubconf.py
+++ hubconf.py
@@ -7,11 +7,13 @@ model_map = {
     'landscapes': 'https://drive.google.com/uc?id=14UGDDOusZ9TMb-pOrF0PAjMGVWLSAii1'
 }
 
-def styleganv3_inference(model: str = 'art'):
-    if model.startswith('http'):
-        url = model
-    elif model in model_map:
-        url = model_map.get(model, None)
+def styleganv3(pretrained: str = 'art'):
+    if pretrained.startswith('https://'):
+        url = pretrained
+    elif pretrained in model_map:
+        url = model_map.get(pretrained, None)
+    elif '/' in pretrained and len(pretrained.split('/')) == 2:
+        url = 'https://hf.co/{pretrained}/resolve/main/model.pkl'
     else:
-        raise ValueError(f'Model {model} not found')
+        raise ValueError(f'Model {pretrained} not found')
     return StyleGAN3ImageGenerationPipeline(url)
