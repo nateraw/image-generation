43057bfe5d3137cb3d52510fd5b821790a491fa4
nateraw <nxr9266@g.rit.edu>
2021-11-29 15:16:01 -0500

    :construction: wip

--- hubconf.py
+++ hubconf.py
@@ -2,10 +2,17 @@ import gdown
 
 from huggingface_stylegan3 import StyleGAN3ImageGenerationPipeline
 
-model_map = {
+stylegan_url = 'https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/'
+
+nvidia_models = ['stylegan3-t-afhqv2-512x512.pkl', 'stylegan3-r-afhqv2-512x512.pkl']
+_model_map = {
     'art': 'https://drive.google.com/uc?id=18MOpwTMJsl_Z17q-wQVnaRLCUFZYSNkj',
-    'landscapes': 'https://drive.google.com/uc?id=14UGDDOusZ9TMb-pOrF0PAjMGVWLSAii1'
+    'landscapes': 'https://drive.google.com/uc?id=14UGDDOusZ9TMb-pOrF0PAjMGVWLSAii1',
+    **{m: f'{stylegan_url}{m}' for m in nvidia_models},
 }
+
+def model_map():
+    return _model_map
 
 def styleganv3(pretrained: str = 'art'):
     if pretrained.startswith('https://'):
