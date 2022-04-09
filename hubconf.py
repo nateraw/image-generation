97d42125c85f565bc97da17f2bc20f98b9b3b25d
Nathan Raw <nxr9266@g.rit.edu>
2022-04-09 00:04:16 -0400

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -21,7 +21,15 @@ nvidia_models = [
 _model_map = {
     'wikiart-1024': 'https://archive.org/download/wikiart-1024-stylegan3-t-17.2Mimg/wikiart-1024-stylegan3-t-17.2Mimg.pkl',
     'landscapes-256': 'https://archive.org/download/lhq-256-stylegan3-t-25Mimg/lhq-256-stylegan3-t-25Mimg.pkl',
-    'vivid-flowers': 'https://www.dropbox.com/s/o33lhgnk91hstvx/network-snapshot-000069.pkl?dl=1',
+    'mechanical-devices-256': 'https://www.dropbox.com/s/v2oie53cz62ozvu/network-snapshot-000029.pkl?dl=1',
+    'vivid-flowers-256': 'https://www.dropbox.com/s/o33lhgnk91hstvx/network-snapshot-000069.pkl?dl=1',
+    'alien-with-sunglasses-256': 'https://www.dropbox.com/s/gur14k0e7kspguy/network-snapshot-000038.pkl?dl=1',
+    'forest-daemons-256': 'https://www.dropbox.com/s/26muctr2eq4br6l/network-snapshot-000018.pkl?dl=1',
+    'scifi-city-256': 'https://www.dropbox.com/s/1kfsmlct4mriphc/network-snapshot-000210.pkl?dl=1',
+    'scifi-spaceship-256': 'https://www.dropbox.com/s/02br3mjkma1hubc/network-snapshot-000162.pkl?dl=1',
+    'yellow-comic-alien-256': 'https://www.dropbox.com/s/g14lsdbohuk2oco/network-snapshot-000019.pkl?dl=1',
+    'yellow-comic-alien-512': 'https://www.dropbox.com/s/yzraojzmg2kybjx/network-snapshot-000236.pkl?dl=1',
+    'mechanical-landscape-256': 'https://www.dropbox.com/s/5oar443k7iw3qvp/network-snapshot-000006.pkl?dl=1',
     **{m: f'{stylegan_url}{m}' for m in nvidia_models},
 }
 
